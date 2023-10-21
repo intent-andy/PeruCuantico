c905a8ef2fbe9e3c4cb5ed55abb02cd56cfddb68
Andrés Caña <148162548+intent-andy@users.noreply.github.com>
2023-10-21 13:46:05 -0400

    Add files via upload

---
+++ Test for CO-Afina 2023/Assets/Scripts/Controlador2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador2 : MonoBehaviour
+{
+    public GameObject Luz;
+    void Start()
+    {
+       Luz.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    public void Luz1()
+    {
+        if(Luz !=null)
+        {
+            bool IsActive = Luz.activeSelf;
+            Luz.SetActive(!IsActive);
+
+        }
+    }
+}
